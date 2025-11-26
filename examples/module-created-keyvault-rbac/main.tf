097a6e199d84149f58c809793dc637b23137d104
andrewbearsley <andrew.bearsley@gmail.com>
2025-11-26 13:22:49 +1100

    Update example region to Australia East (Sydney)

--- examples/module-created-keyvault-rbac/main.tf
+++ examples/module-created-keyvault-rbac/main.tf
@@ -4,7 +4,7 @@ module "lacework_azure_agentless_scanning" {
   integration_level                    = "SUBSCRIPTION"
   global                               = true
   create_log_analytics_workspace       = true
-  region                               = "West US 2"
+  region                               = "Australia East"
   included_subscriptions               = ["/subscriptions/subscription-1", "/subscriptions/subscription-2"]
   key_vault_enable_rbac_authorization  = true
   key_vault_enable_purge_protection    = true
